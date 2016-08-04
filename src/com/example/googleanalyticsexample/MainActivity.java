7fcf801bb7374920846cf36f608d3a5074e46e82
Jeonggyu <goodsogi@gmail.com>
2016-08-04 16:07:53 +0900

    first commit

---
+++ src/com/example/googleanalyticsexample/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.googleanalyticsexample;
+
+import com.google.analytics.tracking.android.EasyTracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		// The rest of your onStart() code.
+		EasyTracker.getInstance(this).activityStart(this); // Add this method.
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		// The rest of your onStop() code.
+		EasyTracker.getInstance(this).activityStop(this); // Add this method.
+	}
+
+}
